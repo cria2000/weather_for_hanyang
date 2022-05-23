4b00b0558c38779d3e4eea198211ae30ee24f292
2018008722 <cria2000@naver.com>
2022-05-23 13:15:11 +0900

    220523 1시 update / 키워드만 선택해도 되도록 변경

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -15,8 +15,8 @@ export const MainPage = (props: Props) => {
 
     return (
         <div className="main_page">
-            <MainWeather mainWeather={0} secondWeather={12}/>
-            <MainClothes top={1} pants={4} bringOuter topPercentage={10} pantsPercentage={10} bringOuterPercentage={10}/>
+            <MainWeather mainWeather={0} secondWeather={2}/>
+            <MainClothes top={1} pants={6} bringOuter topPercentage={63} pantsPercentage={38} bringOuterPercentage={63}/>
             <button className="weather_button" onClick={handleClickButton}>{messages.askOpinion}</button>
         </div>
     );
