1277b7f5135704babc9f49d649d691b87983a32c
2018008722 <cria2000@naver.com>
2022-05-24 12:52:42 +0900

    220524 1시 update

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -15,8 +15,8 @@ export const MainPage = (props: Props) => {
 
     return (
         <div className="main_page">
-            <MainWeather mainWeather={12} secondWeather={0}/>
-            <MainClothes top={1} pants={5} bringOuter={false} topPercentage={50} pantsPercentage={50} bringOuterPercentage={50}/>
+            <MainWeather mainWeather={0} secondWeather={12}/>
+            <MainClothes top={13} pants={6} bringOuter={false} topPercentage={40} pantsPercentage={40} bringOuterPercentage={60}/>
             <button className="weather_button" onClick={handleClickButton}>{messages.askOpinion}</button>
         </div>
     );
